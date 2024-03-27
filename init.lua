aa95c1c00a4261d6d700f1e35c59b19c92d4dc1e
Marco <marco.marchiante@gmail.com>
2024-03-27 11:53:42 +0100

    added basic golang support

--- init.lua
+++ init.lua
@@ -537,7 +537,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -829,6 +829,20 @@ require('lazy').setup({
     'kaarmu/typst.vim',
     ft = 'typst',
     lazy = false,
+  },
+  {
+    'ray-x/go.nvim',
+    dependencies = { -- optional packages
+      'ray-x/guihua.lua',
+      'neovim/nvim-lspconfig',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('go').setup()
+    end,
+    event = { 'CmdlineEnter' },
+    ft = { 'go', 'gomod' },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
 }, {
   ui = {
